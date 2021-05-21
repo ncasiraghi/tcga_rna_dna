87943d31624e34aaa6420d67e08cd3bc3301ed3a
Nicola <casiraghinicola@gmail.com>
2021-05-21 08:27:05 +0000

    new folders

---
+++ Pstep_modified_weighted/computeData.R
@@ -0,0 +1,136 @@
+library(data.table)
+library(tidyverse)
+library(parallel)
+
+options(dplyr.summarise.inform = FALSE)
+options(scipen = 9999)
+
+source('/BCGLAB/ncasiraghi/tcga_rna_dna/Pstep_modified_weighted/computeData_functions.R')
+
+args <- commandArgs(trailingOnly = TRUE)
+
+wd <- args[1]
+
+length.sw <- as.numeric(args[2])
+
+pos_step <- as.numeric(args[3])
+
+samples.in.parallel <- as.numeric(args[4])
+
+setwd(wd)
+
+# outs
+
+outdir <- paste0('main_out_afs_sw_',length.sw,'_Pstep_',format(pos_step,scientific = FALSE))
+
+if(!file.exists(file.path(wd,outdir))){
+  dir.create(file.path(wd, outdir), showWarnings = FALSE)
+} else{
+  unlink(file.path(wd,outdir),recursive = TRUE)
+  dir.create(file.path(wd, outdir), showWarnings = FALSE)
+}
+
+# pileup on snps
+
+lf <- list.files('/BCGLAB/2020_signatures/pileup/TCGA-BRCA',pattern = '_DNA',full.names = TRUE)
+
+write(x = basename(lf),file = file.path(wd,outdir,'samples.txt'),ncolumns = 1)
+
+# cytobands
+
+bands <- read.delim('/BCGLAB/ncasiraghi/gene_panel/input/cytobands/ucsc_cytobands_GRCh38p13.tsv',stringsAsFactors = F)
+
+bands$arm <- 'p'
+bands$arm[grep(bands$name,pattern = 'q')] <- 'q'
+
+bands <- bands %>% 
+  filter(!chrom %in% c('chrY','chrM')) %>% 
+  group_by(chrom,arm) %>% 
+  summarise(start = min(chromStart), end = max(chromEnd))
+
+# Positions to aggregate data
+
+arm_borders <- read.delim('/BCGLAB/2020_signatures/stats/modified_Pstep/arm_borders.tsv',stringsAsFactors = F)
+
+arm_borders <- arm_borders %>% 
+  group_by(chr,arm) %>% 
+  summarise(start = min(pos), end = max(pos))
+
+positions <- c()
+
+for(j in seq_len(nrow(arm_borders))){
+  
+  pos <- c(seq(from=arm_borders$start[j],arm_borders$end[j],by = pos_step), arm_borders$end[j])
+  
+  x <- data.frame(chrom=arm_borders$chr[j],
+                  arm=arm_borders$arm[j],
+                  pos=unique(pos),
+                  stringsAsFactors = FALSE)
+  
+  positions <- rbind(positions,x)
+  
+}
+
+# genes of interest
+
+goi <- c(readLines('/BCGLAB/ncasiraghi/gene_panel/input/oncogenes.txt'),
+         readLines('/BCGLAB/ncasiraghi/gene_panel/input/cancer_genes.txt'),
+         readLines('/BCGLAB/ncasiraghi/gene_panel/input/tumor_suppressors.txt')) %>% 
+  unique() %>% 
+  sort()
+
+ensembl <- read.delim('/BCGLAB/ncasiraghi/gene_panel/input/mart_export_GRCh38p13.tsv',check.names = F,stringsAsFactors = F,col.names = c('ensg','start','end','chr','band','Hugo_Symbol'))
+
+ensembl <- ensembl %>% mutate(gene_center = (start + end) %/% 2)
+
+gc <- ensembl %>% filter(Hugo_Symbol %in% goi)
+
+gc$chr <- paste0('chr',gc$chr)
+gc$arm <- 'p'
+gc$arm[grep(gc$band,pattern = 'q')] <- 'q'
+
+positions$dist_cancer_gene <- NA
+
+for(idx in seq_len(nrow(positions))){
+  
+  u <- gc %>% 
+    filter(chr == positions$chrom[idx] & arm == positions$arm[idx])
+  
+  if(nrow(u) > 0){
+    positions$dist_cancer_gene[idx] <- min(abs(positions$pos[idx] - u$gene_center))
+  }
+  
+}
+
+write.table(positions,file = file.path(wd,outdir,'positions.tsv'),col.names = TRUE,quote = FALSE,row.names = FALSE,sep = '\t')
+
+# run sliding window
+
+getMatrix <- function(i,lf,bands,length.sw,positions){
+  
+  id <- lf[i]
+  
+  file <- list.files(id,full.names = TRUE,pattern = '\\.snps$')
+  
+  snps <- fread(file,data.table = FALSE,verbose = FALSE,stringsAsFactors = FALSE) %>% 
+    filter(af >= 0.1, af <= 0.9) %>% 
+    filter(!chr %in% c('chrY','chrM'))
+  
+  tomi <- which(snps$af < 0.5)
+  snps$af[tomi] <- (1 - snps$af[tomi])
+  
+  sl <- snps %>% 
+    group_by(chr) %>% 
+    group_split()
+  
+  deck <- lapply(seq_len(length(sl)), runsw, sl, bands, length.sw = length.sw, positions = positions)
+  
+  return(as.numeric(unlist(deck)))
+  
+}
+
+listmat <- mclapply(seq_len(length(lf)),getMatrix,lf=lf,bands=bands,length.sw=length.sw,positions=positions,mc.cores = samples.in.parallel)
+
+mat <- do.call(rbind,listmat)
+
+write.table(x = mat,file = file.path(wd,outdir,'data.tsv'),sep = '\t',quote = FALSE,col.names = FALSE,row.names = FALSE)
---
+++ Pstep_modified_weighted/computeData_functions.R
@@ -0,0 +1,92 @@
+compute_stats <- function(indx, chr, adf, tab){
+  
+  m <- adf[tab$start[indx]:tab$stop[indx],]
+  
+  out <- data.frame(chr=chr,
+                    arm=unique(adf$arm),
+                    wnd_start=min(m$pos),
+                    wnd_end=max(m$pos),
+                    snp_pos=paste(m$pos,collapse = ';'),
+                    snp_af=paste(m$af,collapse = ';'),
+                    stringsAsFactors = FALSE)
+  return(out)
+}
+
+get_afs <- function(px, stats){
+  
+  x <- stats %>% 
+    filter(wnd_start <= px & wnd_end >= px) 
+  
+  compute_wafs <- function(j,x,px){
+    
+    dst <- abs(as.numeric(unlist(strsplit(x$snp_pos[j],split = ';'))) - px)
+    
+    waf <- weighted.mean(x = as.numeric(unlist(strsplit(x$snp_af[j],split = ';'))), w = 1/dst)
+    
+    return(waf)
+  }
+  
+  wout <- unlist(lapply(seq_len(nrow(x)),compute_wafs,x=x,px=px))
+  
+  return( median(wout,na.rm = TRUE) )
+  
+}
+
+stats_by_arm <- function(df,chr,which.arm,wnd,positions){
+  
+  adf <- df %>% filter(arm == which.arm)
+  
+  if( nrow(adf) == 0 ){
+    
+    stats <- data.frame(chr=chr,
+                        arm=which.arm,
+                        wnd_start=NA,
+                        wnd_end=NA,
+                        snp_pos=NA,
+                        snp_af=NA,
+                        stringsAsFactors = FALSE)
+    
+  } else {
+    
+    if( wnd <= nrow(adf) ){
+      start <- 1:(nrow(adf)-(wnd-1))
+      
+      stop <- start+(wnd-1)
+      
+      tab <- data.frame(start = start, stop = stop, stringsAsFactors = FALSE)
+    } else {
+      tab <- data.frame(start = 1, stop = nrow(adf))
+    }
+    
+    stats <- lapply(seq_len(nrow(tab)), compute_stats, chr = chr, adf = adf, tab = tab)
+    stats <- do.call(rbind,stats)
+  }
+  
+  pos.arm <- as.numeric(positions %>% filter(arm == which.arm, chrom == chr ) %>% pull(pos))
+  
+  afs <- lapply(pos.arm, get_afs, stats = stats)
+  afs <- as.numeric(unlist(afs))
+  
+  return(afs)
+  
+}
+
+runsw <- function(i, sl, bands, length.sw, positions){
+  
+  df <- sl[[i]]
+  df$arm <- NA
+  
+  chr <- unique(df$chr)
+  
+  p.coord <- bands %>% filter(chrom == chr, arm == 'p')
+  df$arm[which( df$pos >= p.coord$start & df$pos <= p.coord$end )] <- 'p'
+  
+  q.coord <- bands %>% filter(chrom == chr, arm == 'q')
+  df$arm[which( df$pos >= q.coord$start & df$pos <= q.coord$end )] <- 'q'
+  
+  out <- c(stats_by_arm(df,chr = chr, which.arm = 'p',wnd = length.sw, positions = positions),
+           stats_by_arm(df,chr = chr, which.arm = 'q',wnd = length.sw, positions = positions))
+  
+  return(out)
+  
+}
