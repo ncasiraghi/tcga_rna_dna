84974a6fc9fc74382a0e9ed77556dae7391b1e4c
Nicola <casiraghinicola@gmail.com>
2021-08-02 10:50:11 +0200

    update auto compute data script

--- Pstep_modified_weighted/create_autoComputeData.R
+++ Pstep_modified_weighted/create_autoComputeData.R
@@ -2,31 +2,26 @@ library(dplyr)
 
 setwd('/BCGLAB/ncasiraghi/tcga_rna_dna/Pstep_modified_weighted')
 
-pileup <- list.files('/BCGLAB/2020_signatures/pileup',full.names = TRUE)
-
 rscript <- 'Rscript /BCGLAB/ncasiraghi/tcga_rna_dna/Pstep_modified_weighted/computeData.R'
-pileup <- grep(pileup,pattern = 'TCGA',value = TRUE)
-type <- c('DNA','RNA')
-aggregate_as <- c('mean','median')
+
+sif.file <- '/BCGLAB/ncasiraghi/tcga_rna_dna/data/datasetSNPs.tsv'
+
+sif <- read.delim(file = sif.file,header = TRUE,stringsAsFactors = FALSE)
+
+cat(unique(sif$data.source),sep = '\n')
+
+dataset <- 'WES-Plasma_TISSUES'
+omic <- 'DNA'
+aggregate_as <- 'mean'
 min_vaf <- 0.1
 max_vaf <- 0.9
-min_cov <- c(0,10)
-sw <- c(25,50,100)
-pstep <- c(0.25,0.75,1)
-n <- 50
+min_cov <- 10
+sw <- c(25,50)
+pstep <- 0.75
+cores <- 50
 
-df <- expand.grid(rscript,pileup,type,aggregate_as,min_vaf,max_vaf,min_cov,sw,pstep,n,stringsAsFactors = FALSE)
+df <- expand.grid(rscript,sif.file,omic,dataset,aggregate_as,min_vaf,max_vaf,min_cov,sw,pstep,cores,stringsAsFactors = FALSE)
 
-colnames(df) <- c('rscript','pileup','type','aggregate_as','min_vaf','max_vaf','min_cov','sw','pstep','n')
+colnames(df) <- c('rscript','sif.file','omic','dataset','aggregate_as','min_vaf','max_vaf','min_cov','sw','pstep','cores')
 
-list.files('/BCGLAB/2020_signatures/input_data',pattern = '^data.tsv$',recursive = TRUE,full.names = TRUE)
-
-m <- df %>% 
-  filter(type == 'DNA') %>% 
-  filter(pstep == 0.75) %>% 
-  filter(aggregate_as == 'median') %>%
-  arrange(pileup)
-
-m$out <- file.path('/BCGLAB/2020_signatures/input_data',basename(m$pileup))
-
-write.table(m, file = 'auto_computeData.sh',quote = FALSE,sep = ' ',col.names = FALSE,row.names = FALSE)
+write.table(df, file = 'auto_computeData.sh',quote = FALSE,sep = ' ',col.names = FALSE,row.names = FALSE)
