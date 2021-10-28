e9f67dfde7360dc26ca9a4bcac5748320af555e5
ncasiraghi <casiraghinicola@gmail.com>
2021-10-28 14:36:32 +0200

    compute not mirrored AFs

--- synthetic_dilutions/check_informative_snps.R
+++ synthetic_dilutions/check_informative_snps.R
@@ -1,5 +1,8 @@
 library(tidyverse)
 library(data.table)
+library(parallel)
+
+mc.cores <- 10
 
 setwd('/BCGLAB/2020_signatures/synthetic_dilutions/pacbam/outs/')
 
@@ -55,11 +58,11 @@ m <- df %>%
 
 getInfoSNPs <- function(i,case.snps,ctrl.snps,patient.id){
   
-  snps <- fread(case.snps$snps[i],data.table = FALSE,showProgress = FALSE,nThread = 2) %>% 
+  snps <- fread(case.snps$snps[i],data.table = FALSE,showProgress = FALSE,nThread = mc.cores) %>% 
     filter(cov >= 10) %>% 
     filter(rsid %in% unique(ctrl.snps$rsid)) %>% 
     select(rsid,af,cov) %>% 
-    mutate(af = if_else(condition = af < 0.5,true = 1 - af,false = af)) %>% 
+    mutate(af.mir = if_else(condition = af < 0.5,true = 1 - af,false = af)) %>% 
     mutate(patient = patient.id, tc = case.snps$tc[i])
   
   return(snps)
@@ -71,22 +74,28 @@ mt <- c()
 for(i in seq_len(length(m))){
   
   ptnt <- m[[i]]
+  cat(paste("[",Sys.time(),"]",unique(ptnt$patient),"\n"))
   
-  message(unique(ptnt$patient))
-  
+  cat(paste("[",Sys.time(),"]","Reading SNPs of the NORMAL sample","\n"))
   ctrl.snps <- ptnt %>% 
     filter(is.na(tc)) %>%
     pull(snps) %>% 
-    fread(.,data.table = FALSE,showProgress = FALSE,nThread = 2) %>% 
+    fread(.,data.table = FALSE,showProgress = FALSE,nThread = mc.cores) %>% 
     filter(cov >= 10) %>% 
     select(rsid,af,cov) %>% 
     filter(af >= 0.1 & af <= 0.9) %>%
-    mutate(af = if_else(condition = af < 0.5,true = 1 - af,false = af)) %>% 
+    mutate(af.mir = if_else(condition = af < 0.5,true = 1 - af,false = af)) %>% 
     mutate(patient = unique(ptnt$patient), tc = NA)
   
   case.snps <- ptnt %>% filter(!is.na(tc))
   
-  out <- lapply(seq_len(nrow(case.snps)),getInfoSNPs, case.snps=case.snps, ctrl.snps=ctrl.snps, patient.id = unique(ptnt$patient)) %>% do.call(rbind,.)
+  cat(paste("[",Sys.time(),"]","Reading SNPs of the TUMOR samples","\n"))
+  
+  out <- lapply(seq_len(nrow(case.snps)),getInfoSNPs, case.snps=case.snps, ctrl.snps=ctrl.snps, patient.id = unique(ptnt$patient))
+  
+  cat(paste("[",Sys.time(),"]","Merging together NORMAL and TUMOR data","\n"))
+  
+  out <- do.call(rbind,out)
   
   mt <- rbind(mt,ctrl.snps,out)
   
@@ -108,3 +117,10 @@ p <- ggplot(mt, aes(x=af,y = as.factor(tc))) +
   facet_wrap(~patient,scales = 'free_y',nrow = 1) + ylab('Tumor content') + theme_bw()
 
 ggsave(filename = 'infosnps.pdf',plot = p, width = 300,height = 150,dpi = 300,units = 'mm',device = 'pdf')
+
+p <- ggplot(mt, aes(x=af.mir,y = as.factor(tc))) +
+  geom_violin() +
+  geom_boxplot(width=0.1) +
+  facet_wrap(~patient,scales = 'free_y',nrow = 1) + ylab('Tumor content') + theme_bw()
+
+ggsave(filename = 'infosnps_AFmir.pdf',plot = p, width = 300,height = 150,dpi = 300,units = 'mm',device = 'pdf')
